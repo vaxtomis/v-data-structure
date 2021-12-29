a6511c416de45ecab7fc6c2bb3ff1bac43d22436
vaxtomis <1051551978@qq.com>
2021-12-29 09:28:00 +0800

    RedBlackTree

--- src/test/java/tree/RedBlackTreeTest.java
+++ src/test/java/tree/RedBlackTreeTest.java
@@ -15,15 +15,15 @@ public class RedBlackTreeTest {
         RedBlackTree<Integer, String> tree = new RedBlackTree<>();
         LinkedList<Integer> list = new LinkedList<>();
         Random random = new Random();
-        for (int i = 0; i < 100; i++) {
-            int randomNum = random.nextInt(1000);
+        for (int i = 0; i < 10; i++) {
+            int randomNum = random.nextInt(100);
             list.add(randomNum);
             tree.insert(randomNum, String.valueOf(randomNum));
         }
 
         System.out.println(Arrays.toString(list.toArray()));
         for (int i : list) {
-            System.out.println(tree.find(i));
+            System.out.print("[" + tree.find(i) + "] ");
         }
     }
 }
